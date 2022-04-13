42c813d7d0834f1c10e1a12ef6491d5a49a83e06
nitinainani <nitinainani@gmail.com>
2022-04-12 20:32:12 -0500

    Add project files.

---
+++ BankConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace BankConsoleApp {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello Nitin");
+
+            var bank = new Bank().SetBankName("Nitin's Bank").AddAccounts(new List<IAccount> { 
+            
+                new CheckingAccount(1, 500),
+                new IndividualInvestmentAcct(2, 2000),
+                new CorporateInvestmentAcct(3,10000)
+            });
+
+
+            // deposit
+            bank.PerformDepositOrWithdrawal(TransactionType.Deposit, 600, 1);
+            Console.Write(bank.CheckBalance(1));
+
+
+            bank.PerformDepositOrWithdrawal(TransactionType.Deposit, 700, 2);
+            Console.Write(bank.CheckBalance(2));
+
+            bank.PerformDepositOrWithdrawal(TransactionType.Deposit, 1700, 3);
+            Console.Write(bank.CheckBalance(3));
+
+
+            //withdraw
+            bank.PerformDepositOrWithdrawal(TransactionType.Withdraw, 100, 1);//1000 balance
+            bank.PerformDepositOrWithdrawal(TransactionType.Withdraw, 150, 2); // 2550 balance
+            bank.PerformDepositOrWithdrawal(TransactionType.Withdraw, 700, 3); // 11000 balance
+        }
+    }
+}
